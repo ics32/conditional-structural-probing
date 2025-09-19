fbe5bbb6ea996afa79e0600f20ad5b018633cab7
ics32 <ics32@cornell.edu>
2025-09-19 13:57:57 -0400

    Added TwoWordPSDProbe from structural probes to probe.py

--- vinfo/probe.py
+++ vinfo/probe.py
@@ -17,6 +17,50 @@ class Probe(nn.Module, InitYAMLObject):
       total_params += numpy.prod(param.size())
     tqdm.write('Probe has {} parameters'.format(total_params))
 
+class TwoWordPSDProbe(Probe):
+  """ Computes squared L2 distance after projection by a matrix.
+
+  For a batch of sentences, computes all n^2 pairs of distances
+  for each sentence in the batch.
+  """
+  yaml_tag = '!TwoWordPSDProbe'
+  def __init__(self, args, model_dim, probe_rank, zero_features=False):
+    print('Constructing TwoWordPSDProbe')
+    super(TwoWordPSDProbe, self).__init__()
+    self.model_dim = model_dim
+    self.probe_rank = probe_rank
+    self.linear = nn.Linear(self.model_dim, self.label_space_size)
+    #self.linear2 = nn.Linear(self.label_space_size, self.label_space_size)
+    self.print_param_count()
+    dropout = .0
+    self.dropout = nn.Dropout(p=dropout)
+    print('Applying dropout {}'.format(dropout))
+    self.zero_features = zero_features
+    self.to(args['device'])
+
+  def forward(self, batch):
+    """ Computes all n^2 pairs of distances after projection
+    for each sentence in a batch.
+
+    Note that due to padding, some distances will be non-zero for pads.
+    Computes (B(h_i-h_j))^T(B(h_i-h_j)) for all i,j
+
+    Args:
+      batch: a batch of word representations of the shape
+        (batch_size, max_seq_len, representation_dim)
+    Returns:
+      A tensor of distances of shape (batch_size, max_seq_len, max_seq_len)
+    """
+    transformed = torch.matmul(batch, self.proj)
+    batchlen, seqlen, rank = transformed.size()
+    transformed = transformed.unsqueeze(2)
+    transformed = transformed.expand(-1, -1, seqlen, -1)
+    transposed = transformed.transpose(1,2)
+    diffs = transformed - transposed
+    squared_diffs = diffs.pow(2)
+    squared_distances = torch.sum(squared_diffs, -1)
+    return squared_distances
+
 class OneWordLinearLabelProbe(Probe):
   """ Computes a linear function of each word vector.
 
