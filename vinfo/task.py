60ef0f2a6df551daa6da81b819efbc06d11d4c05
ics32 <ics32@cornell.edu>
2025-09-19 14:45:03 -0400

    Made a ParseDistanceClass task in task.py

--- vinfo/task.py
+++ vinfo/task.py
@@ -158,6 +158,138 @@ class TokenClassificationTask(InitYAMLObject):
       labels[token_index] = self.category_int_of_label_string(label_string)
     return labels
 
+class ParseDistanceTask(InitYAMLObject):
+  """Maps observations to dependency parse distances between words."""
+
+  
+  yaml_tag = '!TokenClassificationTask'
+  train_cache = None
+  dev_cache = None
+  test_cache = None
+
+  def __init__(self, args, task_name, input_fields, cache=None):
+    """
+    Args:
+     - task_name: the string identifier for the task.
+                  e.g., the field name in the ontonotes
+                  annotation that provides labels for this task
+    """
+    self.task_name = task_name
+    #self.name_to_index_dict = {name:i for i, name in enumerate(args['input_fields'])}
+    self.label_vocab = {'[PAD]':0, '-':0, '_':0}
+    self.ints_to_strings = {}
+    #self.cache = cache
+    self.cache = None
+    self.input_fields = input_fields
+    self.task_name = task_name
+    self.name_to_index_dict = None
+
+
+  def setup_cache(self):
+    """Constructs reader of or writer to disk cache
+
+    If cache is exists and is valid, constructs a reader of the cache,
+    otherwise constructs a writer to cache features as they're constructed
+    """
+    train_cache_path = self.cache.get_cache_path_and_check(TRAIN_STR, self.task_name)
+    dev_cache_path = self.cache.get_cache_path_and_check(DEV_STR, self.task_name)
+    test_cache_path = self.cache.get_cache_path_and_check(TEST_STR, self.task_name)
+
+    self.train_cache_writer = None
+    self.dev_cache_writer = None
+    self.test_cache_writer = None
+
+    if os.path.exists(train_cache_path):
+      f = h5py.File(train_cache_path, 'r')
+      self.train_cache = (torch.tensor(f[str(i)][()]) for i in range(len(f.keys())))
+    else:
+      self.train_cache_writer = h5py.File(train_cache_path, 'w')
+    if os.path.exists(dev_cache_path):
+      f2 = h5py.File(dev_cache_path, 'r')
+      self.dev_cache = (torch.tensor(f2[str(i)][()]) for i in range(len(f2.keys())))
+    else:
+      self.dev_cache_writer = h5py.File(dev_cache_path, 'w')
+    if os.path.exists(test_cache_path):
+      f3 = h5py.File(test_cache_path, 'r')
+      self.test_cache = (torch.tensor(f3[str(i)][()]) for i in range(len(f3.keys())))
+    else:
+      self.test_cache_writer = h5py.File(test_cache_path, 'w')
+  def labels(observation):
+    """Computes the distances between all pairs of words; returns them as a torch tensor.
+
+    Args:
+      observation: a single Observation class for a sentence:
+    Returns:
+      A torch tensor of shape (sentence_length, sentence_length) of distances
+      in the parse tree as specified by the observation annotation.
+    """
+    sentence_length = len(observation[0]) #All observation fields must be of same length
+    distances = torch.zeros((sentence_length, sentence_length))
+    for i in range(sentence_length):
+      for j in range(i,sentence_length):
+        i_j_distance = ParseDistanceTask.distance_between_pairs(observation, i, j)
+        distances[i][j] = i_j_distance
+        distances[j][i] = i_j_distance
+    return distances
+
+  @staticmethod
+  def distance_between_pairs(observation, i, j, head_indices=None):
+    '''Computes path distance between a pair of words
+
+    TODO: It would be (much) more efficient to compute all pairs' distances at once;
+          this pair-by-pair method is an artefact of an older design, but
+          was unit-tested for correctness... 
+
+    Args:
+      observation: an Observation namedtuple, with a head_indices field.
+          or None, if head_indies != None
+      i: one of the two words to compute the distance between.
+      j: one of the two words to compute the distance between.
+      head_indices: the head indices (according to a dependency parse) of all
+          words, or None, if observation != None.
+
+    Returns:
+      The integer distance d_path(i,j)
+    '''
+    if i == j:
+      return 0
+    if observation:
+      head_indices = []
+      number_of_underscores = 0
+      for elt in observation.head_indices:
+        if elt == '_':
+          head_indices.append(0)
+          number_of_underscores += 1
+        else:
+          head_indices.append(int(elt) + number_of_underscores)
+    i_path = [i+1]
+    j_path = [j+1]
+    i_head = i+1
+    j_head = j+1
+    while True:
+      if not (i_head == 0 and (i_path == [i+1] or i_path[-1] == 0)):
+        i_head = head_indices[i_head - 1]
+        i_path.append(i_head)
+      if not (j_head == 0 and (j_path == [j+1] or j_path[-1] == 0)):
+        j_head = head_indices[j_head - 1]
+        j_path.append(j_head)
+      if i_head in j_path:
+        j_path_length = j_path.index(i_head)
+        i_path_length = len(i_path) - 1
+        break
+      elif j_head in i_path:
+        i_path_length = i_path.index(j_head)
+        j_path_length = len(j_path) - 1
+        break
+      elif i_head == j_head:
+        i_path_length = len(i_path) - 1
+        j_path_length = len(j_path) - 1
+        break
+    total_length = j_path_length + i_path_length
+    return total_length
+
+
+
 class CoarseTokenClassificationTask(TokenClassificationTask):
   yaml_tag = '!CoarseTokenClassificationTask'
   train_cache = None
